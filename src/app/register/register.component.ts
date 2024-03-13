3c6e3be76dc3046f864b7219cd7f406aa149d9de
Makinen-Tayler <makinen.tayler@outlook.com>
2024-03-13 10:06:50 -0600

    talk to web api

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,20 +1,52 @@
 import { Component } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, Validators, ReactiveFormsModule, FormControl, FormGroup} from '@angular/forms';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { UserService } from '../services/user.service';
+
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [MatInputModule, MatFormFieldModule, FormsModule, MatCardModule, MatButtonModule, MatIconModule, RouterModule],
+  imports: [MatInputModule, MatFormFieldModule, FormsModule, MatCardModule, MatButtonModule, MatIconModule, RouterModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 
 
+
 export class RegisterComponent {
 
+  constructor(private service: UserService) {}
+
+  registerForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
+
+
+  onSubmit() {
+    if(this.registerForm.valid) {
+      console.log("submitted form");
+      console.log(this.registerForm.value)
+      this.service.register(this.registerForm.value).subscribe(res => {
+        console.log(res);
+      })
+      this.registerForm.reset();
+
+      // Clear validation states
+      Object.keys(this.registerForm.controls).forEach(controlName => {
+        this.registerForm.get(controlName)?.clearValidators();
+        this.registerForm.get(controlName)?.updateValueAndValidity();
+      });
+
+    }
+    else {
+      alert("please fill out all fields to sign up!")
+    }
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  apiUrl: string = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  register(formData: any) {
+    return this.http.post<any>(`${this.apiUrl}/User/register`, formData)
+  }
 }
